f92b6c964995509a0035dd1d6b84ee5eb720ea53
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-02 18:12:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,7 +22,7 @@ def reduce_to_total(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-    source_array.reduce{ |a, b| a + b }
+    source_array.reduce(starting_point){ |a, b| a + b }
 end
 
 
