7d80369e21197262f26a037a03256e22b9d89cd2
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-02 17:06:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,12 +16,12 @@ def map_to_square(source_array)
 end
 
 # REDUCE FUNCTIONS
-def reduce_to_total(source_array, starting_point)
-  source_array.reduce(starting_point) {|a, b| a + b}
-end
-
 def reduce_to_total(source_array)
   source_array.reduce { |a, b| a + b }
+end
+
+def reduce_to_total(source_array, starting_point)
+  source_array.reduce(starting_point) {|a, b| a + b}
 end
 
 
