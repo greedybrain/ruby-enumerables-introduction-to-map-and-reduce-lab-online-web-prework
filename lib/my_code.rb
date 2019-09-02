902a9881a1d19feefd79090970854b90edf2dcea
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-02 17:18:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,7 +28,7 @@ def reduce_to_any_true(source_array)
   source_array.reduce{|i| i ? i : !i}
 end
 
-def reduce_to_any_true(source_array)
+def reduce_to_all_false(source_array)
   source_array.reduce{|i| i ? !i : i}
 end
 
