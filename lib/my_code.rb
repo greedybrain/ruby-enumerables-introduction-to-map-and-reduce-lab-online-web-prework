4e0a1ea03d7e8862f458ae48bd10c0201023be2e
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-02 18:45:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,7 +17,7 @@ end
 
 # REDUCE FUNCTIONS
 
-def reduce_to_total(source_array, starting_point = 0)
+def reduce_to_total(source_array, starting_point=0)
   new = starting_point
   i = 0
   while i < source_array.length do
@@ -27,10 +27,23 @@ def reduce_to_total(source_array, starting_point = 0)
   return new
 end
 
-# def reduce_to_total(source_array, starting_point)
-#     source_array.reduce(starting_point){ |a, b| a + b }
-# end
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
+end
 
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i]
+    i += 1
+  end
+  return false
+end
 
 
 
